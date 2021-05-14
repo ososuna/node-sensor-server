e35df4ca40f57fa17cd18e47116642bfaf93e8a9
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-05-14 16:29:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 
 require('dotenv').config();
 const Server = require('./models/server');
-/*
-const Serial = require('./models/serial');
-const serial = new Serial();
-serial.data();
-*/
 const server = new Server();
 server.listen();
 server.getData();
